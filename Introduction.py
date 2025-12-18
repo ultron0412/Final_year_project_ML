575b19492bb3cd8991d01a817832c9a1812913eb
Aayush Jung Kunwar <paban.piyush@gmail.com>
2025-12-18 12:17:23 +0545

    Introduction File

---
+++ Introduction.py
@@ -0,0 +1,3 @@
+print("Hello Everyone This is my Final year project that i have worked in for my 4th year 8th sem final major project.")
+print("This Project is prepared for the requirement of final year project.")
+print("This project is of weightage of 3 credit.")
